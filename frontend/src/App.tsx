335182937268c499d06cafcd560611c189924037
Paulo Moraes <paulomoraesx1@gmail.com>
2021-11-02 15:47:15 -0300

    componentes basicos estaticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">olá mundo</h1>
+    <>
+    <NavBar/>
+    <div className="container">
+      <h1 className="text-primary">Tabela de Dados</h1>
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/paulomoraesx" target="_blank" rel="noreferrer">Paulo Moraes</a></p>
+            </div>
+        </footer>
+    )
+}
