6e253fdcd87216b91dc8dede5fb9c44055a29bdb
Paulo Moraes <paulomoraesx1@gmail.com>
2021-11-09 17:01:16 -0300

    home, navegacoes

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import work from 'assets/img/work.jpg'
+import { Link } from 'react-router-dom'
 
 export default function NavBar() {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={work} alt="DevSuperior" width="120" />
+                    <Link to="/">
+                        <img src={work} alt="DevSuperior" width="120" />
+                    </Link>
                 </nav>
             </div>
         </div>
