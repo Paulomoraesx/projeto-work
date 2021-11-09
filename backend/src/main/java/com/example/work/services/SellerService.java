fac7aa4a673d6eac62dc8bf799ac09780866df8f
Paulo Moraes <paulomoraesx1@gmail.com>
2021-11-09 15:03:54 -0300

    camadas

---
+++ backend/src/main/java/com/example/work/services/SellerService.java
@@ -0,0 +1,26 @@
+package com.example.work.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.work.dto.SellerDTO;
+import com.example.work.entities.Seller;
+import com.example.work.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result =  repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+	
+	
+
+}
